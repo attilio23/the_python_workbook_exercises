08ec19d5f59fc09923f1ffb3357f4385b2470fd7
attilio23 <81169300+attilio23@users.noreply.github.com>
2021-04-20 15:50:59 +0200

    Update exercise_77.py

--- chapter_3/exercise_77.py
+++ chapter_3/exercise_77.py
@@ -1,5 +1,5 @@
 ##
-#Display a multiplication table that shows the products of all combinations of integers from 1 times 1 up to and including 10 times 10 
+#Display the multiplication table that shows the products of all combinations of integers from 1 times 1 up to and including 10 times 10 
 print("  ",end='')
 #Display the numbers from 1 to 10
 for i in range(1,11):
